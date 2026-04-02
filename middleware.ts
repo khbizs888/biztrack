5c3af27b07a9a35e575125493de274efebcafc03
hoho <khbizs888@gmail.com>
2026-04-02 17:16:35 +0800

    fix: remove middleware logic to resolve edge runtime crash

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,5 @@
-import { type NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function middleware() {
   return NextResponse.next()
-}
-
-export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
 }
